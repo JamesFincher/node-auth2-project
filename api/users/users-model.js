f717e3aaa79076f79e0d822a765ca52d78a04214
James Fincher <heartandkarma@gmail.com>
2022-05-24 12:45:42 -0700

    wip

--- api/users/users-model.js
+++ api/users/users-model.js
@@ -45,20 +45,15 @@ function findBy(filter) {
     return db('users')
       .join('roles', 'users.role_id', 'roles.role_id')
       .select('user_id', 'username', 'password', 'role_name')
+      .where(filter)
 }
 
 
 function findById(user_id) {
-  /**
-    You will need to join two tables.
-    Resolves to the user with the given user_id.
-
-    {
-      "user_id": 2,
-      "username": "sue",
-      "role_name": "instructor"
-    }
-   */
+  return db('users')
+    .join('roles', 'users.role_id', 'roles.role_id')
+    .select('user_id', 'username', 'password', 'role_name')
+    .where('users.user_id', user_id).first()
 }
 
 /**
